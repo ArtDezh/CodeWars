5bbcaf341e92d2873e3c4285308cb671b53c5581
Arthur <artur2134@mail.ru>
2024-10-31 23:55:07 +0300

    new task

---
+++ src/find/the/odd/iint/FindOdd.java
@@ -0,0 +1,35 @@
+package find.the.odd.iint;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class FindOdd {
+    public static void main(String[] args) {
+        int[] a = {20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5};
+        System.out.println(findIt(a));
+    }
+
+    public static int findIt(int[] a) {
+        Map<Integer, Integer> hp = new HashMap<>();
+        for (int i = 0; i < a.length; i++) {
+            hp.put(a[i], hp.getOrDefault(a[i], 0) + 1);
+        }
+
+        int result = 0;
+        Set<Integer> keys = hp.keySet();
+        for (Integer i: keys) {
+            result = hp.get(i);
+            if (result % 2 !=0) {
+                break;
+            }
+        }
+        /*Optional<Map.Entry<Integer, Integer>> result = hp.entrySet()
+                .stream()
+                .filter(entry -> entry.getKey() % 2 != 0)
+                .max(Map.Entry.comparingByKey());
+        if (result.isPresent()) return result.get().getValue();
+        else return 0;*/
+        return result;
+    }
+}
