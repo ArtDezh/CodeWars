08df783c8fc792e44ab0968c2024d322de2e5a0f
Arthur <artur2134@mail.ru>
2024-10-31 10:47:20 +0300

    fixed

--- src/who/likes/it/Solution.java
+++ src/who/likes/it/Solution.java
@@ -11,19 +11,19 @@ public class Solution {
         switch (names.length) {
 
             case 0:
-                result = "no one likes it";
+                result = "no one likes this";
                 break;
             case 1:
                 result = names[0] + " likes this";
                 break;
             case 2:
-                result = names[0] + " and " + names[1] + " likes this";
+                result = names[0] + " and " + names[1] + " like this";
                 break;
             case 3:
-                result = names[0] + ", " + names[1] + " and " + names[2] + " likes this";
+                result = names[0] + ", " + names[1] + " and " + names[2] + " like this";
                 break;
             default:
-                result = names[0] + ", " + names[1] + " and " + (names.length - 2) + " others likes this";
+                result = names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
         }
         return result;
     }
