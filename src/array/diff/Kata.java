71aac04482726855515904bcececcf2cdeb55252
Arthur <artur2134@mail.ru>
2024-11-01 10:29:21 +0300

    done

--- src/array/diff/Kata.java
+++ src/array/diff/Kata.java
@@ -1,7 +1,30 @@
 package array.diff;
 
+import java.util.*;
+
 public class Kata {
     public static void main(String[] args) {
-        
+        int[] arrA = {1,2,3,4,5};
+        int[] arrB = {2};
+        System.out.println(Arrays.toString(arrayDiff(arrA, arrB)));
+    }
+
+    public static int[] arrayDiff(int[] a, int[] b) {
+        Set<Integer> list = new HashSet<>();
+
+        for (int i: a) {
+            list.add(i);
+            for (int j: b) {
+                list.add(j);
+            }
+        }
+
+        int[] result = new int[list.size()];
+        int count = 0;
+        for(Integer i: list) {
+            result[count] = (int) i;
+            count++;
+        }
+        return result;
     }
 }
