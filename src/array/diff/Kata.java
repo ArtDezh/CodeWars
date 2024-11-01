2959e4fdebe5edb98fa9262458acbd8efe0bef60
Arthur <artur2134@mail.ru>
2024-11-01 23:18:09 +0300

    done

--- src/array/diff/Kata.java
+++ src/array/diff/Kata.java
@@ -4,27 +4,11 @@ import java.util.*;
 
 public class Kata {
     public static void main(String[] args) {
-        int[] arrA = {1,2,3,4,5};
-        int[] arrB = {2};
-        System.out.println(Arrays.toString(arrayDiff(arrA, arrB)));
+        System.out.println(Arrays.toString(arrayDiff(new int[] {1, 2, 2, 2, 3}, new int[] {2})));
     }
 
     public static int[] arrayDiff(int[] a, int[] b) {
-        Set<Integer> list = new HashSet<>();
 
-        for (int i: a) {
-            list.add(i);
-            for (int j: b) {
-                list.add(j);
-            }
-        }
-
-        int[] result = new int[list.size()];
-        int count = 0;
-        for(Integer i: list) {
-            result[count] = (int) i;
-            count++;
-        }
-        return result;
+        return Arrays.stream(a).filter(i -> Arrays.stream(b).noneMatch(j -> i == j)).toArray();
     }
 }
