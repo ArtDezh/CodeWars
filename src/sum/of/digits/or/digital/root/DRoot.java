b937d52eefd98e239720f0dbaa67e96da1c270ea
Arthur <artur2134@mail.ru>
2024-11-02 10:13:07 +0300

    done

--- src/sum/of/digits/or/digital/root/DRoot.java
+++ src/sum/of/digits/or/digital/root/DRoot.java
@@ -1,7 +1,24 @@
 package sum.of.digits.or.digital.root;
 
 public class DRoot {
+
     public static void main(String[] args) {
-        
+        System.out.println(digital_root(12345));
+    }
+
+    public static int digital_root(int n) {
+        while (n >= 10) {
+            n = digital_sum(n);
+        }
+        return n;
+    }
+
+    public static int digital_sum(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 }
