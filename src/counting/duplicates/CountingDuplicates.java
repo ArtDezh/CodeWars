94aa32763f1e729ad48ce8582bc48cff51e7c7dc
Arthur <artur2134@mail.ru>
2024-11-05 21:31:25 +0300

    new task

---
+++ src/counting/duplicates/CountingDuplicates.java
@@ -0,0 +1,11 @@
+package counting.duplicates;
+
+public class CountingDuplicates {
+    public static void main(String[] args) {
+
+    }
+
+    public static int duplicateCount(String text) {
+        return 0;
+    }
+}
