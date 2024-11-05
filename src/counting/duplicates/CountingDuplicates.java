7467a50f24c985b3fbf4cc251153c680bc5c83cd
Arthur <artur2134@mail.ru>
2024-11-05 21:53:45 +0300

    Done

--- src/counting/duplicates/CountingDuplicates.java
+++ src/counting/duplicates/CountingDuplicates.java
@@ -1,11 +1,27 @@
 package counting.duplicates;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountingDuplicates {
     public static void main(String[] args) {
-
+        System.out.println(duplicateCount("abbdrreThgt"));
     }
 
     public static int duplicateCount(String text) {
-        return 0;
+        Map<Character, Integer> mapChars = new HashMap<>();
+        int result = 0;
+        for (char c : text.toLowerCase().toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                mapChars.put(c, mapChars.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : mapChars.entrySet()) {
+            if (entry.getValue() > 1) {
+                result++;
+            }
+        }
+        return result;
     }
 }
