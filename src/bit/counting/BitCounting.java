4be71014405c6bed33fabf2682c4f0a91e4bec00
Arthur <artur2134@mail.ru>
2024-11-06 20:18:01 +0300

    Done

--- src/bit/counting/BitCounting.java
+++ src/bit/counting/BitCounting.java
@@ -2,10 +2,12 @@ package bit.counting;
 
 public class BitCounting {
     public static void main(String[] args) {
-
+        System.out.println(countBits(1234));
     }
 
     public static int countBits(int n) {
-        return 0;
+        String binaryRepresentation = Integer.toBinaryString(n);
+        
+        return (int) binaryRepresentation.chars().filter(c -> c == '1').count();
     }
 }
