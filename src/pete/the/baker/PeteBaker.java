8cadeb153e1e80d63f7ac474af70b24678e3c3bf
Arthur <artur2134@mail.ru>
2024-11-06 20:28:33 +0300

    New task

---
+++ src/pete/the/baker/PeteBaker.java
@@ -0,0 +1,14 @@
+package pete.the.baker;
+
+import java.util.Map;
+
+public class PeteBaker {
+    public static void main(String[] args) {
+
+    }
+
+    public static int cakes(Map<String, Integer> recipe, Map<String, Integer> available) {
+        // TODO: Insert Code Here
+        return -1;
+    }
+}
