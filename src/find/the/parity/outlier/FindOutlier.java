3aa8aa2e016cd2bdb5a1b3d0079593656333c30b
Arthur <artur2134@mail.ru>
2024-11-07 21:46:03 +0300

    New task

---
+++ src/find/the/parity/outlier/FindOutlier.java
@@ -0,0 +1,11 @@
+package find.the.parity.outlier;
+
+public class FindOutlier {
+    public static void main(String[] args) {
+
+    }
+
+    static int find(int[] integers) {
+        return 0;
+    }
+}
--- src/pete/the/baker/PeteBaker.java
+++ src/pete/the/baker/PeteBaker.java
@@ -19,7 +19,7 @@ public class PeteBaker {
     public static int cakes(Map<String, Integer> recipe, Map<String, Integer> available) {
         int maxCakes = Integer.MAX_VALUE;
 
-        for (Map.Entry<String, Integer> ingredient: recipe.entrySet()) {
+        for (Map.Entry<String, Integer> ingredient : recipe.entrySet()) {
             String ingredientName = ingredient.getKey();
             int requiredAmount = ingredient.getValue();
 
